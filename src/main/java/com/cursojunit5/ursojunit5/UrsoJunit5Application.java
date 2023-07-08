3e89f60855c76dd6e49e3c9c4408881aac7b9265
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2023-07-08 14:24:47 -0300

    first commit

---
+++ src/main/java/com/cursojunit5/ursojunit5/UrsoJunit5Application.java
@@ -0,0 +1,13 @@
+package com.cursojunit5.ursojunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrsoJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrsoJunit5Application.class, args);
+	}
+
+}
