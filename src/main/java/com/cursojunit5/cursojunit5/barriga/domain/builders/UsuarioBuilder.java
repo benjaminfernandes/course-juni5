aecbd9b46a5fcaee6b2eda7eff6e590bf7487996
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2023-09-30 12:16:16 -0300

    Criando BuilderMaster

--- src/main/java/com/cursojunit5/cursojunit5/barriga/domain/builders/UsuarioBuilder.java
+++ src/main/java/com/cursojunit5/cursojunit5/barriga/domain/builders/UsuarioBuilder.java
@@ -3,50 +3,48 @@ package com.cursojunit5.cursojunit5.barriga.domain.builders;
 import com.cursojunit5.cursojunit5.barriga.domain.Usuario;
 
 public class UsuarioBuilder {
-
 	private Integer id;
 	private String nome;
 	private String email;
 	private String senha;
-	
-	private UsuarioBuilder() {
-	}
-	
+
+	private UsuarioBuilder(){}
+
 	public static UsuarioBuilder umUsuario() {
 		UsuarioBuilder builder = new UsuarioBuilder();
-		iniciarlizarDadosPadroes(builder);
+		inicializarDadosPadroes(builder);
 		return builder;
 	}
 
-	private static void iniciarlizarDadosPadroes(UsuarioBuilder builder) {
-		builder.id = 1;
+	private static void inicializarDadosPadroes(UsuarioBuilder builder) {
+		builder.id = 0;
 		builder.nome = "Usuario Valido";
 		builder.email = "user@mail.com";
 		builder.senha = "123456";
 	}
-	
-	public UsuarioBuilder comId(int param) {
-		id = param;
+
+	public UsuarioBuilder comId(Integer id) {
+		this.id = id;
 		return this;
 	}
-	
-	public UsuarioBuilder comNome(String param) {
-		nome = param;
+
+	public UsuarioBuilder comNome(String nome) {
+		this.nome = nome;
 		return this;
 	}
-	
-	public UsuarioBuilder comEmail(String param) {
-		email = param;
+
+	public UsuarioBuilder comEmail(String email) {
+		this.email = email;
 		return this;
 	}
-	
-	public UsuarioBuilder comSenha(String param) {
-		senha = param;
+
+	public UsuarioBuilder comSenha(String senha) {
+		this.senha = senha;
 		return this;
 	}
-	
+
 	public Usuario agora() {
 		return new Usuario(id, nome, email, senha);
 	}
-	
 }
+
